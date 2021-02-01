fbf632e39735e2846de8979cf20a9ae8fae1df6b
Snehal Ranjan <snehalranjan5472@gmail.com>
2021-02-01 11:48:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0)
+}
+
+function draw() {
+  
+}
+
+function mouseClicked() {
+  stroke(255, 0, 0)
+  strokeWeight(5)
+  fill(0, 0, 255) 
+  
+    circle(mouseX, mouseY, 20)
+
+}
